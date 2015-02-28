efee168b115e5772272433f9fd8f6da3f8e265e0
hakobe <hakobe@gmail.com>
2015-02-28 22:40:13 +0900

    change interface

--- chaining-stars/compiled.js
+++ chaining-stars/compiled.js
@@ -1,4 +1,101 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+global.ChainingStarMouseStalker = require('./chaining-star-mouse-stalker');
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./chaining-star-mouse-stalker":2}],2:[function(require,module,exports){
+'use strict';
+
+var twemoji = require('twemoji');
+var Bacon = require('baconjs');
+
+var animationFrame = Bacon.fromBinder(function(sink) {
+  var unsub;
+  function next(t) {
+    sink(t);
+    unsub = requestAnimationFrame(next);
+  }
+  unsub = requestAnimationFrame(next);
+  return unsub;
+});
+
+function Stalker(targetStream) {
+  var elem = document.createElement('div');
+  elem.style.position = 'absolute';
+  elem.innerHTML = twemoji.parse('\u2b50');
+
+  var position = { x: 400, y: 400 };
+  elem.style.display = 'none';
+  elem.style.left = position.x + 'px';
+  elem.style.top = position.y + 'px';
+
+  document.body.appendChild(elem);
+
+  var initialized = false;
+
+  var targetPosition = {x: 0, y:0};
+  targetStream.onValue(function(p) {
+    if (!initialized) {
+      elem.style.display = '';
+      initialized = true;
+    }
+    targetPosition.x = p.x;
+    targetPosition.y = p.y;
+  });
+
+  var bound = 10;
+
+  function next() {
+    var xDiff = Math.abs(position.x - targetPosition.x);
+    var yDiff = Math.abs(position.y - targetPosition.y);
+
+    var xDir = position.x > targetPosition.x ? 1 : -1;
+    var yDir = position.y > targetPosition.y ? 1 : -1;
+
+    var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+    var progress = Math.min(1.0, Math.max(0.0, distance / bound - 0.3));
+
+    var relativeX = xDir * progress * bound * ( distance === 0 ? 1 : xDiff / distance );
+    var relativeY = yDir * progress * bound * ( distance === 0 ? 1 : yDiff / distance );
+
+    var newX = targetPosition.x + relativeX;
+    var newY = targetPosition.y + relativeY;
+
+    return {x: newX, y: newY};
+  }
+
+  var self = this;
+
+  var ps = new Bacon.Bus();
+  self.positionStream = ps.skipDuplicates();
+
+  animationFrame.onValue(function() {
+    if (initialized) {
+      position = next();
+      ps.push(position);
+      elem.style.left = position.x + 'px';
+      elem.style.top = position.y + 'px';
+    }
+  });
+}
+
+var mouseCursorStream =
+  Bacon.fromEventTarget(document, 'mousemove').map(function(me) {
+    return {
+      x: me.clientX,
+      y: me.clientY
+    };
+  });
+
+exports.start = function() {
+  var cur = new Stalker(mouseCursorStream);
+  for (var i = 0; i < 30; i++) {
+    cur = new Stalker(cur.positionStream.delay(100));
+  }
+};
+
+},{"baconjs":3,"twemoji":4}],3:[function(require,module,exports){
 (function (global){
 (function() {
   var Bacon, BufferingSource, Bus, CompositeUnsubscribe, ConsumingSource, Desc, Dispatcher, End, Error, Event, EventStream, Exception, Initial, Next, None, Observable, Property, PropertyDispatcher, Some, Source, UpdateBarrier, _, addPropertyInitValueToStream, assert, assertArray, assertEventStream, assertFunction, assertNoArguments, assertObservable, assertString, cloneArray, constantToFunction, containsDuplicateDeps, convertArgsToFunction, describe, endEvent, eventIdCounter, eventMethods, findDeps, findHandlerMethods, flatMap_, former, idCounter, initialEvent, isArray, isFieldKey, isObservable, latter, liftCallback, makeFunction, makeFunctionArgs, makeFunction_, makeObservable, makeSpawner, nextEvent, nop, partiallyApplied, recursionDepth, ref, registerObs, spys, toCombinator, toEvent, toFieldExtractor, toFieldKey, toOption, toSimpleExtractor, valueAndEnd, withDescription, withMethodCallSupport,
@@ -3313,7 +3410,7 @@ if ((typeof define !== "undefined" && define !== null) && (define.amd != null)) 
 }).call(this);
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],2:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 (function (global){
 var location = global.location || {};
 /*jslint indent: 2, browser: true, bitwise: true, plusplus: true */
@@ -3836,96 +3933,4 @@ if (!location.protocol) {
 }
 module.exports = twemoji;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],"reactive-mouse-stalker":[function(require,module,exports){
-'use strict';
-
-var twemoji = require('twemoji');
-var Bacon = require('baconjs');
-
-var animationFrame = Bacon.fromBinder(function(sink) {
-  var unsub;
-  function next(t) {
-    sink(t);
-    unsub = requestAnimationFrame(next);
-  }
-  unsub = requestAnimationFrame(next);
-  return unsub;
-});
-
-function Stalker(targetStream) {
-  var elem = document.createElement('div');
-  elem.style.position = 'absolute';
-  elem.innerHTML = twemoji.parse('\u2b50');
-
-  var position = { x: 400, y: 400 };
-  elem.style.display = 'none';
-  elem.style.left = position.x + 'px';
-  elem.style.top = position.y + 'px';
-
-  document.body.appendChild(elem);
-
-  var initialized = false;
-
-  var targetPosition = {x: 0, y:0};
-  targetStream.onValue(function(p) {
-    if (!initialized) {
-      elem.style.display = '';
-      initialized = true;
-    }
-    targetPosition.x = p.x;
-    targetPosition.y = p.y;
-  });
-
-  var bound = 10;
-
-  function next() {
-    var xDiff = Math.abs(position.x - targetPosition.x);
-    var yDiff = Math.abs(position.y - targetPosition.y);
-
-    var xDir = position.x > targetPosition.x ? 1 : -1;
-    var yDir = position.y > targetPosition.y ? 1 : -1;
-
-    var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-
-    var progress = Math.min(1.0, Math.max(0.0, distance / bound - 0.3));
-
-    var relativeX = xDir * progress * bound * ( distance === 0 ? 1 : xDiff / distance );
-    var relativeY = yDir * progress * bound * ( distance === 0 ? 1 : yDiff / distance );
-
-    var newX = targetPosition.x + relativeX;
-    var newY = targetPosition.y + relativeY;
-
-    return {x: newX, y: newY};
-  }
-
-  var self = this;
-
-  var ps = new Bacon.Bus();
-  self.positionStream = ps.skipDuplicates();
-
-  animationFrame.onValue(function() {
-    if (initialized) {
-      position = next();
-      ps.push(position);
-      elem.style.left = position.x + 'px';
-      elem.style.top = position.y + 'px';
-    }
-  });
-}
-
-var mouseCursorStream =
-  Bacon.fromEventTarget(document, 'mousemove').map(function(me) {
-    return {
-      x: me.clientX,
-      y: me.clientY
-    };
-  });
-
-exports.start = function() {
-  var cur = new Stalker(mouseCursorStream);
-  for (var i = 0; i < 30; i++) {
-    cur = new Stalker(cur.positionStream.delay(100));
-  }
-};
-
-},{"baconjs":1,"twemoji":2}]},{},[]);
+},{}]},{},[1]);
